157387e74180e8a2a05185c49530f2de06474ce9
forgelucas <lucasforge1209@gmail.com>
2025-02-12 18:29:11 -0300

    Configurando o Django e o Front da aplicacao

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    titulo = models.CharField(max_length=100)
+    video = models.FileField(upload_to='video/')
+    transcricao = models.TextField(null=True, blank=True)
+    resumo = models.TextField()
+
+    def __str__(self):
+        self.titulo
--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
     if request.method == "GET":
         return render(request, 'home.html')
+    elif request.method == "POST":
+        titulo = request.POST.get('titulo')    
+        video = request.FILES.get('video')
